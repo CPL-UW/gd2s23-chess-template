282163e0d755f9c29aa5f6bd29f5d634e20cbd7c
Matthew Berland <mberland@wisc.edu>
2022-12-12 16:42:13 -0600

    fixed dx

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -1,35 +1,73 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
-using UnityEngine.Serialization;
+using Random = UnityEngine.Random;
 
 // chess x = a-h, y = 1-8
 public class BoardManager : MonoBehaviour
 {
     public List<Piece> p1Pieces;
     public List<Piece> p2Pieces;
-    // public GameObject piecePrefab;
 
-    // void MakeNewPieceFromSprite(Sprite piece)
-    // {
-    //     var newPiece = Instantiate(piecePrefab, transform);
-    //     newPiece.GetComponent<SpriteRenderer>().sprite = piece;
-    //     Debug.Log(newPiece.GetComponent<SpriteRenderer>().sprite.texture);
-    //     newPiece.transform.Translate(Random.Range(-3,3), Random.Range(0, 3), 0);
-    //     newPiece.name = piece.name;
-    // }
-    
-    void Start()
+    private float _screenWidth;
+    private const float TOLERANCE = 0.1f;
+    private Rect _pieceBounds = Rect.zero;
+    private float xMax;
+    private float yMax;
+    private float xMin;
+    private float yMin;
+    private float _dx = 1;
+    private float _dy = 1;
+    private ChessInfo.PieceColor _turn = ChessInfo.PieceColor.WHITE;
+
+
+    private void GetPieceGridBounds()
     {
-        // foreach (var piece in p1Pieces)
-        // {
-        //     
-        // }
+        xMax = xMin = 0;
+        foreach (var p in p1Pieces.Select(piece => piece.transform.position))
+        {
+            if (p.x > xMax)
+                xMax = p.x;
+            if (p.x < xMin)
+                xMin = p.x;
+        }
+
+        _dx = (xMax - xMin) / 7;
+        _dy = _dx;
+        Debug.Log($"BOUNDS_UPDATE: {xMin}=>{xMax} dx={_dx}");
     }
+
+    private Vector2Int GetGridPosition(Vector3 pos)
+    {
+        return new Vector2Int((int) ((_pieceBounds.xMax + pos.x) / _dx), (int) ((_pieceBounds.yMax + pos.y) / _dy));
+    }
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Math.Abs(_screenWidth - Screen.width) > TOLERANCE)
+        {
+            GetPieceGridBounds();
+            _screenWidth = Screen.width;
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            GetPieceGridBounds();
+            if (_turn == ChessInfo.PieceColor.WHITE)
+            {
+                var randomPiece = p1Pieces.ElementAt(Random.Range(0, p1Pieces.Count));
+                randomPiece.transform.Translate(0, _dy, 0);
+                _turn = ChessInfo.PieceColor.BLACK;
+            }
+            else
+            {
+                var randomPiece = p2Pieces.ElementAt(Random.Range(0, p2Pieces.Count));
+                randomPiece.transform.Translate(0, -_dy, 0);
+                _turn = ChessInfo.PieceColor.WHITE;
+            }
+        }
     }
 }
